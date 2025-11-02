c9a5da1ea42ae54763a0293b1fece167291d4f32
Junaid Farooq <fjunaid252@gmail.com>
2025-11-02 13:28:19 +0500

    chore: initialize TimeOS project with dependencies
    - Add ecto_sql, postgrex, jason for core functionality
    - Add ex_machina for test factories
    - Add mock for testing
    - Configure aliases for database tasks

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Timeos.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
@@ -19,11 +21,25 @@ defmodule Timeos.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.10"},
+      {:postgrex, "~> 0.17"},
+      {:jason, "~> 1.4"},
+      {:ex_doc, "~> 0.30", only: :dev},
+      {:ex_machina, "~> 2.7", only: :test},
+      {:mock, "~> 0.3", only: :test}
+    ]
+  end
+
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end
