bb3635e9fb60cc4253147f23e755334c8121098a
Junaid Farooq <fjunaid252@gmail.com>
2025-11-14 17:05:52 +0500

    feat: add graceful shutdown and UI support
    - Implement graceful shutdown waiting for in-flight jobs
    - Add optional web UI startup (configurable via enable_ui)
    - Wait up to 5 seconds for running jobs to complete
    - Log warnings for jobs still running after grace period

--- lib/timeos/application.ex
+++ lib/timeos/application.ex
@@ -5,6 +5,8 @@ defmodule Timeos.Application do
 
   @impl true
   def start(_type, _args) do
+    TimeOS.Telemetry.setup()
+
     children = [
       TimeOS.Repo,
       TimeOS.RuleRegistry,
@@ -16,6 +18,45 @@ defmodule Timeos.Application do
     ]
 
     opts = [strategy: :one_for_one, name: Timeos.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, pid} = Supervisor.start_link(children, opts)
+
+    if Application.get_env(:timeos, :enable_ui, false) do
+      start_ui()
+    end
+
+    {:ok, pid}
+  end
+
+  @impl true
+  def stop(_state) do
+    require Logger
+    Logger.info("Shutting down TimeOS...")
+    wait_for_running_jobs()
+    :ok
+  end
+
+  defp wait_for_running_jobs do
+    require Logger
+    running_count = TimeOS.Health.check().metrics.running_jobs
+
+    if running_count > 0 do
+      Logger.info("Waiting for #{running_count} running jobs to complete...")
+      Process.sleep(5_000)
+
+      remaining = TimeOS.Health.check().metrics.running_jobs
+      if remaining > 0 do
+        Logger.warning("#{remaining} jobs still running after grace period")
+      end
+    end
+  end
+
+  defp start_ui do
+    require Logger
+    case Application.ensure_all_started(:plug_cowboy) do
+      {:ok, _} ->
+        TimeOS.Web.start()
+      _ ->
+        Logger.warning("Plug/Cowboy not available, UI not started")
+    end
   end
 end
