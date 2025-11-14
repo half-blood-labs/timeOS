7a43388ef6f4e8fd2e619872e9f05d346e2bfd49
Junaid Farooq <fjunaid252@gmail.com>
2025-11-14 16:24:16 +0500

    feat: add timezone utilities for timezone-aware scheduling
    - Add to_timezone function for converting UTC to specific timezone
    - Add to_utc function for converting back to UTC
    - Add now_in_timezone helper for current time in timezone

---
+++ lib/timeos/timezone_utils.ex
@@ -0,0 +1,27 @@
+defmodule TimeOS.TimezoneUtils do
+  @moduledoc """
+  Timezone utilities for scheduling.
+  """
+
+  def to_timezone(datetime, nil), do: {:ok, datetime}
+  def to_timezone(datetime, timezone) when is_binary(timezone) do
+    case DateTime.shift_zone(datetime, timezone) do
+      {:ok, dt} -> {:ok, dt}
+      error -> error
+    end
+  end
+  def to_timezone(datetime, _), do: {:ok, datetime}
+
+  def to_utc(datetime) do
+    case DateTime.shift_zone(datetime, "Etc/UTC") do
+      {:ok, dt} -> {:ok, dt}
+      error -> error
+    end
+  end
+
+  def now_in_timezone(timezone) do
+    DateTime.utc_now()
+    |> to_timezone(timezone)
+    |> elem(1)
+  end
+end
